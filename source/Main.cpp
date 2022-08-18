f6eea5bcbc5273bd7078fbe2a60f77e59c4ab999
Squeemos <Squeemos@gmail.com>
2022-08-17 17:11:06 -0700

    Start from scratch

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,31 +1,6 @@
-#include "Trace.h"
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include "Engine.h"
-#include "Error.h"
-
-#include <iostream>
-#include <memory>
 
 // Main
 int main()
 {
-	try
-	{
-		std::unique_ptr<GameEngine> engine = std::make_unique<GameEngine>();
-
-		while (engine->running())
-		{
-			engine->update();
-		}
-	}
-	catch (std::exception& e)
-	{
-		send_trace_message(e.what());
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
+	return 0;
 }
